73428327d0c78e4851bf7fb8948443ae251a00e2
roopadharap <53837232+roopadharap@users.noreply.github.com>
2020-05-12 15:49:42 -0500

    Update singleNonDuplicate.java
    You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.

--- singleNonDuplicate.java
+++ singleNonDuplicate.java
@@ -1,28 +1,19 @@
 public class Solution{
+	//input example : int [] arr = {3,3,6};
+	
+	public int singleNonDuplicate(int[] nums) 
+	{
+		//if array length is equal, there is no single integer or 2 single integers. Return error code
+		if(nums.length % 2 == 0)
+		    return -1;
 
-public int singleNonDuplicate(int[] nums) 
-{
-        //You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
-        
-        if(nums.length % 2 == 0)
-            return -1;
-            
-        for(int i=0; i<nums.length-1; i=i+2)
-        {
-            if(nums[i] != nums[i+1])
-                return nums[i];
-        }
-        
-        return nums[nums.length-1];
-}
+		for(int i=0; i<nums.length-1; i=i+2)
+		{
+		    if(nums[i] != nums[i+1])
+			return nums[i];
+		}
 
-public static void main(String [] args)
-{
-	Solution s = new Solution();
-
-	int [] arr = {3,3,6};
-        int res = s.singleNonDuplicate(arr);
-        System.out.println(res);
-}
+		return nums[nums.length-1];
+	}
 
 }
