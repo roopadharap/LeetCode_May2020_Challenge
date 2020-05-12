27f9d7e013f16dcc07f3841cc53b097624ca1327
roopadharap <53837232+roopadharap@users.noreply.github.com>
2020-05-12 15:57:55 -0500

    Add files via upload

---
+++ singleNonDuplicate.java
@@ -0,0 +1,20 @@
+public class Solution
+{
+
+//input example : int [] arr = {3,3,6};
+
+public int singleNonDuplicate(int[] nums) 
+{
+                
+        if(nums.length % 2 == 0)
+            return -1;
+            
+        for(int i=0; i<nums.length-1; i=i+2)
+        {
+            if(nums[i] != nums[i+1])
+                return nums[i];
+        }
+        
+        return nums[nums.length-1];
+}
+}
